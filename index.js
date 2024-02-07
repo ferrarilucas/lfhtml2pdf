547e388d35ddebacb8a1839b4b9cd4d8e299259e
ferrarilucas <ferrari.lucasr@gmail.com>
2024-02-07 19:53:26 -0300

    Add index introducing the solution and finish the main code

---
+++ index.js
@@ -0,0 +1,27 @@
+import { chromium } from 'playwright';
+async function main(base64String) {
+    const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  // Decodifica a string base64 para HTML
+  const htmlContent = Buffer.from(base64String, 'base64').toString('utf8');
+
+  // Carrega o HTML no navegador headless
+  await page.setContent(htmlContent, { waitUntil: 'networkidle' });
+  const finalHtml = await page.content();
+  const pdf = await page.pdf({ path: "teste.pdf", format: "A4", printBackground: true, margin: { top: "0", right: "0", bottom: "0", left: "0" } });
+
+  console.log(pdf.toString('base64'));
+  await browser.close();
+}
+
+const [,,htmlBase64Input] = process.argv;
+
+if (!htmlBase64Input) {
+  console.log('Por favor, forneça uma string base64 como argumento.');
+  process.exit(1);
+}
+
+main(htmlBase64Input)
+  .then((pdfBase64) => console.log(pdfBase64))
+  .catch((error) => console.error('Erro ao gerar PDF:', error));
